03a3cf4f94a2b35699595e5a9888629985d7c89b
MuhammadAhmadAslam <prince1ahmed111@gmail.com>
2024-07-22 13:18:39 +0500

    com,mit

---
+++ app.js
@@ -0,0 +1,27 @@
+
+function generate() {
+    let hShadow = document.getElementById('h-shadow').value
+    let vShadow = document.getElementById('v-shadow').value
+    let bRadius = document.getElementById('b-radius').value
+    let sRadius = document.getElementById('s-radius').value
+    let sColor = document.getElementById('s-color').value
+    let insetCheck = document.getElementById('inset')
+    let box = document.getElementById('box')
+    let copyArea = document.getElementById('copyArea')
+
+    let shadow = `${insetCheck.checked ? 'inset' : ''} ${hShadow}px ${vShadow}px ${bRadius}px ${sRadius}px ${sColor}`
+    box.style.textShadow = shadow
+
+    copyArea.innerText = `box-shadow:${shadow};`
+}
+function copyText() {
+    let copyText = document.getElementById("copyArea");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); 
+    document.execCommand("copy");
+    Swal.fire({
+        title: "Good job!",
+        text: "You Copied The Text",
+        icon: "success"
+      });
+}
